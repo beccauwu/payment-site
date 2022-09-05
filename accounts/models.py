6f6371e0987a87ad4c351e7ce0837ee755e334bf
beccauwu <rebecca@perttula.co>
2022-09-05 20:26:34 +0200

    🚧 start a profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    stripe_customer_id = models.CharField(max_length=255, blank=True, null=True)
