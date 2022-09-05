3b23f52358ab9642c6c5885ced1790e2e3d99803
beccauwu <rebecca@perttula.co>
2022-09-05 20:26:17 +0200

    improve auth forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm, PasswordResetForm, SetPasswordForm, AuthenticationForm
+from django.contrib.auth import login, authenticate
 from django.forms import ModelForm
 
 class LoginForm(AuthenticationForm):
@@ -10,6 +11,11 @@ class LoginForm(AuthenticationForm):
         super().__init__(*args, **kwargs)
         self.fields['username'].widget.attrs.update({'class': "form-control giBold text-center"})
         self.fields['password'].widget.attrs.update({'class': "form-control giBold text-center"})
+    
+    def user_login(self):
+        user = authenticate(username=self.cleaned_data['username'], password=self.cleaned_data['password'])
+        if user is not None:
+            login(self.request, user)
 
 class SignupForm(UserCreationForm):
     class Meta:
@@ -18,9 +24,10 @@ class SignupForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['password'].widget.attrs.update({'class': 'form-control'})
-        self.fields['username'].widget.attrs.update({'class': 'form-control'})
-        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control text-center', 'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control text-center', 'placeholder': 'Confirm Password'})
+        self.fields['username'].widget.attrs.update({'class': 'form-control text-center', 'placeholder': 'Username'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control text-center', 'placeholder': 'Email'})
 
 class CustomPasswordChangeForm(PasswordChangeForm):
     class Meta:
@@ -29,8 +36,9 @@ class CustomPasswordChangeForm(PasswordChangeForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field in self.fields:
-            self.fields[field].widget.attrs.update({'class': 'form-control'})
+        self.fields['old_password'].widget.attrs.update({'class': 'form-control'})
+        self.fields['new_password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['new_password2'].widget.attrs.update({'class': 'form-control'})
 
 class CustomPasswordResetForm(PasswordResetForm):
     class Meta:
@@ -40,6 +48,13 @@ class CustomPasswordResetForm(PasswordResetForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['email'].widget.attrs.update({'class': 'form-control'})
+    def get_users(self, email):
+        users = super().get_users(email)
+        try:
+            user = next(users)
+            return [user]
+        except StopIteration:
+            return []
 
 class PasswordSetForm(SetPasswordForm):
     class Meta:
