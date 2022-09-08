7e2a2399c1f7969a9e3fa59036a09110f1576512
beccauwu <rebecca@perttula.co>
2022-09-08 19:40:59 +0200

    ✨ include api urls

--- shop/urls.py
+++ shop/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('purchases/', include('payments.urls')),
     path('', include('general.urls')),
     path('user/', include('accounts.urls')),
+    path('api/', include('api.urls')),
 ]
