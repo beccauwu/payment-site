c6f6b566c3bca851132351ad6ab8aa8973a22857
beccauwu <rebecca@perttula.co>
2022-09-08 19:38:01 +0200

    ✨ work on payment views

--- payments/views.py
+++ payments/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.shortcuts import redirect
 from django.views import View
 from django.views.generic import TemplateView
-from payments.models import Product, Price, OrderItem
+from payments.models import Product, Price, OrderItem, Order
 import general.stripe_stuff as stripe_funcs
 
 class CreateCheckoutSessionView(View):
@@ -19,13 +19,25 @@ class CreateCheckoutSessionView(View):
                 "quantity": int(item["quantity"])
             })
         print(line_items)
-        return redirect(stripe_funcs.create_session(line_items))
+        session = stripe_funcs.create_session(line_items)
+        self.request.session['stripe_session_id'] = session.id
+        return redirect(session.url)
 
 class SuccessView(TemplateView):
     template_name = "success.html"
     def get_context_data(self, **kwargs):
         context = super(SuccessView, self).get_context_data(**kwargs)
-        items = stripe_funcs.empty_basket(self.request.user)
+        order = Order.objects.get(
+            session=self.request.session.session_key,
+            completed=False
+            )
+        if self.request.user.is_authenticated:
+            order.user = self.request.user
+        items = stripe_funcs.empty_basket(order)
+        order.completed = True
+        order.stripe_session_id = self.request.session['stripe_session_id']
+        order.save()
+        self.request.session['stripe_session_id'] = None
         context.update({'items': items})
         self.request.session['items'].clear()
         return context
