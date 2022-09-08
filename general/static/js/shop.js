c89474778f55f95363653302ef9842ecd8df9bde
beccauwu <rebecca@perttula.co>
2022-09-08 20:31:27 +0200

    🚧 work on javascript file for shop

--- general/static/js/shop.js
+++ general/static/js/shop.js
@@ -1,6 +1,6 @@
 const prodAPI = "http://127.0.0.1:8000/api/products/";
 const basketAPI = "http://127.0.0.1:8000/api/basket/";
-const prodhtml = (img, name, id) => {
+const prodhtml = (img, name, id, q, s) => {
   return `
 <div class="product col rounded bg-light wi-fc text-center">
     <div class="description mx-auto" style="width:100px;">
@@ -9,54 +9,71 @@ const prodhtml = (img, name, id) => {
     <div class="text-center">
         <p class="d-inline giBold">${name}</p>
     </div>
-    <p class="d-inline giBold">in basket:</p>
-    <div class="input-group">
+    <div class="input-group wi-fc mx-auto">
         <span class="input-group-btn">
             <button type="button" onclick="subtract_basket(${id})" class="btn btn-default btn-number" data-type="minus" data-field="quant[1]">
                 <span class="fa-solid fa-minus"></span>
             </button>
         </span>
-        <input type="text" id="basket-counter-${id}" value="0" max="100" onchange="set_basket(${id})" class="d-inline giBold form-control counter"></input>
+        <input type="text" size="${s}" id="basket-counter-${id}" value="${q}" max="100" onchange="set_basket(${id})"
+            class="d-inline text-center giBold form-control counter" style="flex:none;width:fit-content;"></input>
         <span class="input-group-btn">
             <button type="button" onclick="add_basket(${id})" class="btn btn-default btn-number" data-type="plus" data-field="quant[1]">
                 <span class="fa-solid fa-plus"></span>
             </button>
         </span>
     </div>
-    <button onclick="add_basket(${id})" id="basket-add-${id}" class="btn btn-sm btn-primary">Add to cart</button>
 </div>
 `;
 };
+let basket;
+const get_quantity = (id) => {
+    if (basket) {
+        return basket[id].quantity;
+    }
+    return 0;
+};
 let allprods;
-let products;
-function get() {
-  $.getJSON(prodAPI, function (results) {
-    products = results;
-    console.log(results);
-    $.each(results, function (index) {
-      let prod = prodhtml(
-        results[index].img,
-        results[index].name,
-        results[index].id
-        );
-      if (allprods == null) {
-        allprods = prod;
-      } else {
-      allprods += prod;
-        }
-      $("#products").html(allprods);
-    });
+function get_basket() {
+  $.ajax({
+    url: basketAPI,
+    type: "GET",
+    dataType: "json",
+    success: function (results) {
+      basket = results;
+    },
   });
 }
-function resizeInput() {
-  $(this).attr("size", $(this).val().length);
+function get() {
+  $.getJSON(prodAPI, function (results) {
+    console.log(results);
+    $.each(results, function (index) {
+        let quantity = get_quantity(results[index].id)
+        console.log(`quantity: ${quantity}`);
+        let prod = prodhtml(
+            results[index].img,
+            results[index].name,
+            results[index].id,
+            quantity,
+            String(quantity).length
+            );
+        if (allprods == null) {
+            allprods = prod;
+        } else {
+        allprods += prod;
+            }
+        $("#products").html(allprods);
+    });
+  });
 }
 function add_basket(id) {
     $.ajax({
         url: basketAPI + id,
         type: "POST",
         success: function (data) {
-        $("#basket-counter-" + id).val(String(data[id].quantity));
+        $("#basket-counter-" + id)
+          .val(String(data[id].quantity))
+          .attr("size", String(data[id].quantity).length);
         }
     });
 }
@@ -67,7 +84,9 @@ function subtract_basket(id) {
         type: "POST",
         data: { 'subtract': 'True' },
         success: function (data) {
-        $("#basket-counter-" + id).val(String(data[id].quantity));
+        $("#basket-counter-" + id)
+          .val(String(data[id].quantity))
+          .attr("size", String(data[id].quantity).length);
         }
     });
 }
@@ -78,28 +97,17 @@ function set_basket(id) {
         type: "POST",
         data: { 'quantity': $("#basket-counter-" + id).val() },
         success: function (data) {
-        $("#basket-counter-" + id).val(String(data[id].quantity));
+        $("#basket-counter-" + id)
+          .val(String(data[id].quantity))
+          .attr("size", String(data[id].quantity).length);
         }
     });
 }
-function get_basket() {
-  $.ajax({
-    url: basketAPI,
-    type: "GET",
-    success: function (results) {
-        console.log(results);
-      $(results).each(function (keyName, keyValue) {
-        console.log(keyName, keyValue);
-        $("#basket-counter-" + keyName).val(String(keyValue.quantity));
-      });
-    },
-  });
-}
 
-$('input[type="text"]').keyup(resizeInput).each(resizeInput);
+
 
 function main() {
-  get();
-  get_basket();
+    get_basket();
+    get();
 }
 main();
