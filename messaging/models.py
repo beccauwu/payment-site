97299b3375a03cb3909e4910c19676d86e435509
beccauwu <rebecca@perttula.co>
2022-09-13 21:10:09 +0200

    ⚰️ remove unused admin panels

--- messaging/models.py
+++ messaging/models.py
@@ -7,9 +7,8 @@ class Message(models.Model):
     recipient = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='recipient')
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, blank=True, default='s')
 
     def __str__(self):
         return f'{self.sender} - {self.recipient} - {self.date}'
 
-    def get_absolute_url(self):
-        return reverse('messaging:messages', kwargs={'pk': self.pk})
--- shop/asgi.py
+++ shop/asgi.py
@@ -10,7 +10,22 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+from rest_live.routers import RealtimeRouter
+from api import views as msg_views
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop.settings')
 
-application = get_asgi_application()
+router = RealtimeRouter()
+router.register(msg_views.MessageViewSet)
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+            path('ws/messages', router.as_consumer().as_asgi(), name='messages'),
+            
+        ])
+    ),
+})
