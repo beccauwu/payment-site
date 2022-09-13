46f3375d23c44d30d5181d99552b2bb6aa0a3a8c
beccauwu <rebecca@perttula.co>
2022-09-13 21:06:47 +0200

    🚧 work on ajax functions

--- general/static/js/shop.js
+++ general/static/js/shop.js
@@ -11,14 +11,14 @@ const prodhtml = (img, name, id, q, s) => {
     </div>
     <div class="input-group wi-fc mx-auto">
         <span class="input-group-btn">
-            <button type="button" onclick="subtract_basket(${id})" class="btn btn-default btn-number" data-type="minus" data-field="quant[1]">
+            <button type="button" onclick="subBasket(${id})" class="btn btn-default btn-number" data-type="minus" data-field="quant[1]">
                 <span class="fa-solid fa-minus"></span>
             </button>
         </span>
-        <input type="text" size="${s}" id="basket-counter-${id}" value="${q}" max="100" onchange="set_basket(${id})"
+        <input type="text" size="${s}" id="basket-counter-${id}" value="${q}" max="100" onchange="setBasket(${id})"
             class="d-inline text-center giBold form-control counter" style="flex:none;width:fit-content;"></input>
         <span class="input-group-btn">
-            <button type="button" onclick="add_basket(${id})" class="btn btn-default btn-number" data-type="plus" data-field="quant[1]">
+            <button type="button" onclick="addBasket(${id})" id="basket-add-${id}" class="btn btn-default btn-number" data-type="plus" data-field="quant[1]">
                 <span class="fa-solid fa-plus"></span>
             </button>
         </span>
@@ -28,86 +28,74 @@ const prodhtml = (img, name, id, q, s) => {
 };
 let basket;
 const get_quantity = (id) => {
-    if (basket) {
-        return basket[id].quantity;
-    }
-    return 0;
+  if (basket) {
+    return basket[id].quantity;
+  }
+  return 0;
 };
 let allprods;
-function get_basket() {
-  $.ajax({
-    url: basketAPI,
-    type: "GET",
-    dataType: "json",
-    success: function (results) {
-      basket = results;
-    },
-  });
-}
 function get() {
   $.getJSON(prodAPI, function (results) {
     console.log(results);
     $.each(results, function (index) {
-        let quantity = get_quantity(results[index].id)
+        const id = results[index].id;
+        let quantity = get_quantity(results[index].id);
         console.log(`quantity: ${quantity}`);
         let prod = prodhtml(
             results[index].img,
-            results[index].name,
-            results[index].id,
+            results[index].prod_name,
+            id,
             quantity,
             String(quantity).length
-            );
+        );
         if (allprods == null) {
             allprods = prod;
         } else {
-        allprods += prod;
-            }
+            allprods += prod;
+        }
         $("#products").html(allprods);
+        });
     });
+}
+function addBasket(id) {
+  $.ajax({
+    url: basketAPI + id,
+    type: "POST",
+    success: function (data) {
+      $("#basket-counter-" + id)
+        .val(String(data[id].quantity))
+        .attr("size", String(data[id].quantity).length);
+    },
   });
 }
-function add_basket(id) {
-    $.ajax({
-        url: basketAPI + id,
-        type: "POST",
-        success: function (data) {
-        $("#basket-counter-" + id)
-          .val(String(data[id].quantity))
-          .attr("size", String(data[id].quantity).length);
-        }
-    });
+
+function subBasket(id) {
+  $.ajax({
+    url: basketAPI + id,
+    type: "POST",
+    data: { subtract: "True" },
+    success: function (data) {
+      $("#basket-counter-" + id)
+        .val(String(data[id].quantity))
+        .attr("size", String(data[id].quantity).length);
+    },
+  });
 }
 
-function subtract_basket(id) {
-    $.ajax({
-        url: basketAPI + id,
-        type: "POST",
-        data: { 'subtract': 'True' },
-        success: function (data) {
-        $("#basket-counter-" + id)
-          .val(String(data[id].quantity))
-          .attr("size", String(data[id].quantity).length);
-        }
-    });
+function setBasket(id) {
+  $.ajax({
+    url: basketAPI + id,
+    type: "POST",
+    data: { quantity: $("#basket-counter-" + id).val() },
+    success: function (data) {
+      $("#basket-counter-" + id)
+        .val(String(data[id].quantity))
+        .attr("size", String(data[id].quantity).length);
+    },
+  });
 }
-
-function set_basket(id) {
-    $.ajax({
-        url: basketAPI + id,
-        type: "POST",
-        data: { 'quantity': $("#basket-counter-" + id).val() },
-        success: function (data) {
-        $("#basket-counter-" + id)
-          .val(String(data[id].quantity))
-          .attr("size", String(data[id].quantity).length);
-        }
-    });
-}
-
-
 
 function main() {
-    get_basket();
-    get();
+  get()
 }
 main();
