e4104262adfaab902a7dd24cbf935cd65d8bd10b
beccauwu <rebecca@perttula.co>
2022-09-13 20:59:58 +0200

    🎨 improve url structure

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,18 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('basket/', views.BasketAPI.as_view(), name='basket_api'),
+#     path('basket/<int:pk>', views.BasketAPI.as_view(), name='basket_api'),
+#     path('products/', views.GetProductsAPI.as_view(), name='product_api'),
+#     path('checkout/', views.CheckoutAPI.as_view(), name='checkout_api'),
+#     path('users/', views.UserAPIView.as_view(), name='user_api'),
+#     path('users/create/', views.UserAPIView.as_view(), name='user_api'),
+# ]
+from django.urls import path, include
 
 urlpatterns = [
-    path('basket/', views.BasketAPI.as_view(), name='basket_api'),
-    path('basket/<int:pk>', views.BasketAPI.as_view(), name='basket_api'),
-    path('products/', views.GetProductsAPI.as_view(), name='product_api'),
+    path('shop/', include('api.urlpatterns.shop')),
+    path('auth/', include('api.urlpatterns.auth')),
+    path('msg/', include('api.urlpatterns.msg')),
 ]
