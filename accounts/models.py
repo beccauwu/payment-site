8c118560e6fea20e1b29202b2548454c504718ca
beccauwu <rebecca@perttula.co>
2022-09-13 20:59:35 +0200

    ✨ modify Profile model, create serializer for user creation

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,11 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    postcode = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    country = models.CharField(max_length=100, blank=True)
     stripe_customer_id = models.CharField(max_length=255, blank=True, null=True)
+    def __str__(self):
+        return self.full_name
--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,11 @@
+import email
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from rest_framework import serializers
-from payments.models import Order, OrderItem, Product, Price
+from payments.models import Order, Product, Price
+from messaging.models import Message
+from accounts.models import Profile
+from general import stripe_stuff as stripe
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,4 +18,54 @@ class ProductSerializer(serializers.ModelSerializer):
         return obj.get_image()
     class Meta:
         model = Product
-        fields = ['id', 'name', 'img']
+        fields = ['id', 'prod_name', 'img']
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('stripe_customer_id', 'full_name', 'address', 'postcode', 'city', 'country',)
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(required=True)
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'profile')
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer()
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'password', 'profile',)
+
+    def create(self, validated_data):
+        print(f"validated_data: {validated_data}")
+        profile_data = validated_data['profile']
+        email = validated_data.pop('email')
+        full_name = profile_data.pop('full_name')
+        username = validated_data.pop('username')
+        password = validated_data.pop('password')
+        address = profile_data.pop('address')
+        postcode = profile_data.pop('postcode')
+        city = profile_data.pop('city')
+        country = profile_data.pop('country')
+        customer = stripe.create_customer(email, full_name)
+        user = User.objects.create(
+                username=username,
+                email=email,
+                password=password)
+        user.save()
+        profile = Profile.objects.create(
+            user = user,
+            full_name = full_name,
+            address = address,
+            postcode = postcode,
+            city = city,
+            country = country,
+            stripe_customer_id = customer.id)
+        profile.save()
+        return user
