7c59e55be802182e05b04736a3dac1d71d5e8b0a
beccauwu <rebecca@perttula.co>
2022-09-15 16:38:13 +0200

    ⚰️ delete unused files

--- api/urlpatterns/msg.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .. import views
-
-urlpatterns = [
-    path('view/', views.MessageViewSet.as_view({'get': 'list'}), name='msg_api'),
-]
--- api/urlpatterns/shop.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .. import views
-
-urlpatterns = [
-    path('basket/', views.BasketAPI.as_view(), name='basket_api'),
-    path('basket/<int:pk>', views.BasketAPI.as_view(), name='basket_api'),
-    path('products/', views.GetProductsAPI.as_view(), name='product_api'),
-    path('checkout/', views.CheckoutAPI.as_view(), name='checkout_api'),
-]
