eaa2379558b895bf7368e221b48e93771ba4e40c
beccauwu <rebecca@perttula.co>
2022-09-15 16:29:23 +0200

    🚧 add model for cookie consent

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime, timedelta
 
 # Create your models here.
 
@@ -12,3 +13,18 @@ class Profile(models.Model):
     stripe_customer_id = models.CharField(max_length=255, blank=True, null=True)
     def __str__(self):
         return self.full_name
+
+class CookieConsent(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE, default=None)
+    session = models.CharField(max_length=100, blank=True)
+    consent = models.BooleanField(default=False)
+    date = models.DateTimeField(blank=True, auto_now=True)
+    def __str__(self):
+        return self.has_expired
+    def has_expired(self):
+        """Check if cookie consent expired
+        Returns:
+            bool: True if expired, False if not
+        """
+        expiry = self.date + timedelta(days=365)
+        return expiry <= datetime.now()
