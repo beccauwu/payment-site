3f2b470e2f3381200ac0925f2c792d638fba9d9f
beccauwu <rebecca@perttula.co>
2022-09-22 00:40:15 +0200

    🚀 add review serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,12 @@
 import email
+import math
 from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from payments.models import Order, Product, Price
 from messaging.models import Message
 from accounts.models import Profile, CookieConsent
+from general.models import Review
 from general import stripe_stuff as stripe
 from django.contrib.auth.hashers import make_password
 
@@ -13,16 +15,45 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = '__all__'
 
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ('id', 'user', 'title', 'stars', 'comment', )
+    def validate_product(self, value):
+        if value is None:
+            raise serializers.ValidationError("Product is required")
+        return Product.objects.get(id=value)
+    def create(self, validated_data):
+        request = self.context.get('request')
+        review = Review.objects.create(**validated_data)
+        return review
+
 class ProductSerializer(serializers.ModelSerializer):
     img = serializers.SerializerMethodField('get_img_url')
     price = serializers.SerializerMethodField('get_price')
+    description = serializers.SerializerMethodField('get_desc')
+    reviews = serializers.SerializerMethodField('get_reviews')
+    average_rating = serializers.SerializerMethodField('get_average_rating')
     def get_img_url(self, obj):
         return obj.get_image()
     def get_price(self, obj):
         return Price.objects.get(product=obj).price
+    def get_desc(self, obj):
+        return obj.get_description()
+    def get_reviews(self, obj):
+        reviews = Review.objects.filter(product=obj)
+        if reviews:
+            return ReviewSerializer(reviews, many=True).data
+        return None
+    def get_average_rating(self, obj):
+        reviews = Review.objects.filter(product=obj)
+        ratings = [review.stars for review in reviews]
+        if reviews:
+            return math.ceil(sum(ratings) / len(ratings))
+        return 0
     class Meta:
         model = Product
-        fields = ['id', 'prod_name', 'img', 'price']
+        fields = ['id', 'prod_name', 'img', 'price', 'description', 'reviews']
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
