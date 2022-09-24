89708e02f80de84997ca0fbb1b99662438583cd1
beccauwu <rebecca@perttula.co>
2022-09-24 16:05:01 +0200

    ✨ add view for customers to be added to newsletter

--- api/views/messaging.py
+++ api/views/messaging.py
@@ -1,14 +1,23 @@
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import api_view
+from  django.conf  import settings
+import requests
 from messaging.models import Message
 from rest_framework import permissions
 from ..serializers import MessageSerializer
 
-class MessageViewSet(ModelViewSet):
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    def get_queryset(self):
-        return self.queryset.filter(recipient=self.request.user)
-    def perform_create(self, serializer):
-        serializer.save(sender=self.request.user)
+API_KEY = settings.ANYMAIL.get('MAILGUN_API_KEY')
+
+@api_view(['POST'])
+def add_to_newsletter(request):
+    email = request.data.get('email')
+    if email:
+        response = requests.post(
+            "https://api.mailgun.net/v3/lists/newsletter@mail.perttula.co/members",
+            auth=("api", API_KEY),
+            data={"subscribed": True, "address": email}
+        )
+        return Response(response.json())
+    else:
+        return Response({'error': 'Email not provided'})
